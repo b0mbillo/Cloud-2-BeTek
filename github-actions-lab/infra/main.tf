c8db586e91d81c39bc77bbeae5ddac09461d1e0d
Brando Montoya <BrandoYesid.Montoya@rockwellautomation.com>
2026-04-16 20:10:26 -0500

    Add GitHub Actions workflow for AWS deployment and create initial app structure

---
+++ github-actions-lab/infra/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+terraform {
+    backend "s3" {
+    bucket = "bucket-brandoMontoyastate-terraform" # Debes crearlo manualmente una vez
+    key = "github-actions-demo/terraform.tfstate" # Ruta dentro del bucket
+    region = "us-east-1"
+    encrypt = true
+    }
+}
+
+resource "aws_s3_bucket" "web" {
+    bucket = "bucket-brandoMontoya-terraform" #nombre del bucket que crearas
+}
+
+resource "aws_s3_bucket_website_configuration" "web_config" {
+    bucket = aws_s3_bucket.web.id
+    index_document {
+        suffix = "index.html"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "web_public" {
+    bucket = aws_s3_bucket.web.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+    bucket = aws_s3_bucket.web.id
+    policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+    Effect = "Allow",
+    Principal = "*",
+    Action = "s3:GetObject",
+    Resource = "arn:aws:s3:::${aws_s3_bucket.web.bucket}/*" # política que permite que el bucket actúe como sitio estático
+        }]
+    })
+}
+
+output "bucket_name" {
+    value = aws_s3_bucket.web.bucket
+}
+
+output "website_url" {
+    value = "http://${aws_s3_bucket.web.bucket}.s3-website-us-east-1.amazonaws.com"
+}
